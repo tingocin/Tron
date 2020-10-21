3d81f9cd72d01c2da8fae052e332be7e409192e6
vauxhall <zero.griffin@gmail.com>
2020-10-21 12:34:03 +0200

    Fix parsing

--- Sources/Ads.swift
+++ Sources/Ads.swift
@@ -3,7 +3,8 @@ import Foundation
 public struct Ads {
     public static var serialise: String {
         Rule.all.reduce(into: "[") {
-            $0 += "{" + .init(describing: $1.action) + "," + .init(describing: $1.trigger) + "},"
+            $0 += $0.count > 1 ? "," : ""
+            $0 += "{" + .init(describing: $1.action) + "," + .init(describing: $1.trigger) + "}"
         } + "]"
     }
 }
